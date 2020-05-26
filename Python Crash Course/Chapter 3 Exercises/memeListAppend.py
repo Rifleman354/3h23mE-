f032df8c28ef93fdb99ec5bd272822d0ab132282
Rifleman354 <41891947+Rifleman354@users.noreply.github.com>
2020-05-25 18:24:54 -0700

    Add files via upload

---
+++ Python Crash Course/Chapter 3 Exercises/memeListAppend.py
@@ -0,0 +1,6 @@
+WH40K_Memes = ['Exterminatus', 'Boreale', 'Spess Mehreens']
+WH40K_Memes.append('Admech')
+print('Notable Warhammer 40K meme: ' + WH40K_Memes[0])
+print('Notable Warhammer 40K meme: ' + WH40K_Memes[1])
+print('Notable Warhammer 40K meme: ' + WH40K_Memes[2])
+print('Notable Warhammer 40K meme: ' + WH40K_Memes[3])
---
+++ Python Crash Course/Chapter 3 Exercises/memeSort.py
@@ -0,0 +1,3 @@
+WH40K_Memes = ['Exterminatus', 'Boreale', 'Spess Mehreens']
+WH40K_Memes.sort()
+print(WH40K_Memes)
