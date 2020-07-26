8219535d744a3f5cb800d1ce146f41309b788a82
Rifleman354 <41891947+Rifleman354@users.noreply.github.com>
2020-07-26 01:28:00 -0700

    Delete languagePolling.py

--- Python Crash Course/Chapter 6 Exercises/languagePolling.py
+++
@@ -1,8 +0,0 @@
-favorite_languages = {'jen': 'python', 'sarah': 'c','edward': 'ruby', 'phil': 'python',
-'artemis':'not polled', 'joey':'not polled'}
-
-for name in favorite_languages.keys():
-	if 'not polled' != favorite_languages[name]:
-		print(name.title() + ', thank you for taking the poll!')
-	else:
-		print(name.title() + ', please take the poll!')
