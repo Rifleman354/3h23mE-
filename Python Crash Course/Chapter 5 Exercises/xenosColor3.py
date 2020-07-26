4df8bead5712db6f134013ccc958ba6f0e2ec017
Rifleman354 <41891947+Rifleman354@users.noreply.github.com>
2020-07-26 01:29:49 -0700

    Delete xenosColor3.py

--- Python Crash Course/Chapter 5 Exercises/xenosColor3.py
+++
@@ -1,5 +0,0 @@
-xenos_color = 'green' # Input variable
-if xenos_color == 'green': # This condition is met from the input variable, printing this
-	print('Player 1 just earned 5 points')
-else: # This condition will not be met
-	print('Player 1 just earned 10 points')
