6ffc689e0919da4b2e6b0cfed9b921561c485ad8
Rifleman354 <41891947+Rifleman354@users.noreply.github.com>
2020-07-26 01:26:54 -0700

    Delete massProduction.py

--- Python Crash Course/Chapter 7 Exercises/massProduction.py
+++
@@ -1,3 +0,0 @@
-prompt = input('What vehicle do you want to mass produce Archmagos? \n Type here: ')
-print('\nWe will mass produce the ' + prompt + ' vehicles Archmagos!')
-# Prompts the user on what vehicle to deploy
